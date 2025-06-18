7fffb48b738862bd6bbe35b2612a1c297f9853d3
tulioanesio <tulioluv@gmail.com>
2025-06-18 17:12:14 -0300

    feat: design updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import moment from "moment";
 function App() {
   const [hours, setHours] = useState(moment().format("LTS"));
   moment().format();
-  const day = moment().format("LL");
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -15,12 +14,11 @@ function App() {
   }, []);
 
   return (
-    <main className="min-h-screen flex justify-center items-center bg-[#F9F9F9] font-inter">
-      <div className="w-144 h-64 bg-white border border-gray-200 shadow-lg rounded-xl flex flex-col items-center justify-center space-y-2">
-        <p className="text-gray-700 text-lg">{day}</p>
-        <p className="text-black text-3xl font-semibold tracking-wide">
-          {hours}
-        </p>
+    <main className="min-h-screen">
+      <div className="flex item-center justify-center border w-128 h-64">
+        <div className="flex item-center justify-center border w-128 h-64">
+          <p className="">{hours}</p>
+        </div>
       </div>
     </main>
   );
